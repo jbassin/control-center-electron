6c9bc5c02caedd5b947e4feee97f95e4a372967d
Joshua Bassin <jbassin@bu.edu>
2018-11-08 11:19:19 -0500

    Added an Index view

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p class="title">
+      Insert Title Here!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+};
+</script>
+
+<style scoped>
+
+</style>
