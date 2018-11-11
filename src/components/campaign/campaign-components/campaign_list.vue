8f55403baf590f06070f75220640713b5dc763cc
Joshua Bassin <jbassin@bu.edu>
2018-11-11 18:39:11 -0500

    Added campaign selection

--- src/components/campaign/campaign-components/campaign_list.vue
+++ src/components/campaign/campaign-components/campaign_list.vue
@@ -3,15 +3,33 @@
     <p class="subtitle is-4">
       Available Campaigns:
     </p>
-    <div v-if="this.campaignTitles.length > 0">
-      <table class="table is-bordered is-hoverable is-fullwidth">
-        <tbody>
-        <tr v-for="(camp, index) in this.campaignTitles"
-            :key="index">
+    <div v-if="this.campaigns.length > 0">
+      <table class="table  is-hoverable is-fullwidth">
+        <thead>
           <th>
-            {{ camp }}
+            Campaign Name
           </th>
-        </tr>
+          <th>
+            Campaign ID
+          </th>
+          <th></th>
+        </thead>
+        <tbody>
+          <tr v-for="(campaign, index) in this.campaigns"
+              :key="index"
+              :class="{ 'is-selected': campaign.name === selectedCampaign.name }"
+              @click="selectedCampaign =  campaign">
+            <td>
+              {{ campaign.name }}
+            </td>
+            <td>
+              {{ campaign.id }}
+            </td>
+            <td class="has-text-centered">
+              <a class="delete"
+                 @click="$emit('delete-campaign', campaign.id)"></a>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
@@ -52,14 +70,24 @@
 export default {
   name: 'ccCampaignList',
   props: {
-    campaignTitles: { required: true },
+    campaignDirectories: { required: true },
   },
   data() {
     return {
       selectedCampaign: '',
     };
   },
-  methods: {
+  computed: {
+    campaigns() {
+      const fs = require('fs');
+      const dndDirectory = `${require('os').homedir()}/.dnd`;
+
+      const campaigns = [];
+      this.campaignDirectories.forEach((dir) => {
+        campaigns.push(JSON.parse(fs.readFileSync(`${dndDirectory}/${dir}/campaign.info`, 'utf-8')));
+      });
+      return campaigns;
+    },
   },
 };
 </script>
--- src/components/campaign/campaign.vue
+++ src/components/campaign/campaign.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="container">
+    <ccConfirmModal title="Confirm Deletion"
+                    contents="Are you sure you want to delete this campaign? This action is irreversible!"
+                    bad="Yes, Delete"
+                    good="No, Don't Delete"
+                    v-if="modal.visible"
+                    @chose="deleteCampaign"/>
     <div class="notification">
       <div v-if="checkState('campaign-selector')">
         <ccCampaignList @campaign-creator="setState('campaign-creator')"
-                        :campaign-titles="refreshDirectories()"/>
+                        @delete-campaign="confirmDeleteCampaign"
+                        :campaign-directories="refreshDirectories()"/>
       </div>
       <div v-if="checkState('campaign-creator')">
         <ccCampaignCreator @campaign-selector="setState('campaign-selector')"
@@ -19,19 +26,29 @@
 
 import ccCampaignList from './campaign-components/campaign_list.vue';
 import ccCampaignCreator from './campaign-components/campaign_creator.vue';
+import ccConfirmModal from '../misc/shared/confirm_modal.vue';
+
 
 export default {
   name: 'ccCampaign',
   components: {
-    ccCampaignList, ccCampaignCreator,
+    ccCampaignList, ccCampaignCreator, ccConfirmModal,
   },
   data() {
     return {
       currentState: 'campaign-selector',
       currentDirectories: [],
+      modal: {
+        visible: false,
+        returnStatus: '',
+      },
     };
   },
   methods: {
+    confirmDeleteCampaign(campaignIndex) {
+      this.modal.visible = true;
+      this.modal.returnStatus = campaignIndex;
+    },
     refreshDirectories() {
       const fs = require('fs');
       const dndDirectory = `${require('os').homedir()}/.dnd`;
@@ -75,6 +92,19 @@ export default {
         fs.writeFileSync(`${campaignDirectory}/characters/${character.id}.info`, characterJson);
       });
       this.setState('campaign-selector');
+    },
+    deleteCampaign(decision) {
+      if (decision === 'good') {
+        this.modal.visible = false;
+        this.modal.returnStatus = '';
+        return;
+      }
+      const fs = require('fs-extra');
+      const campaignDirectory = `${require('os').homedir()}/.dnd/${this.modal.returnStatus}`;
+      fs.removeSync(campaignDirectory);
+      this.refreshDirectories();
+      this.modal.visible = false;
+      this.modal.returnStatus = '';
     },
   },
   computed: {
